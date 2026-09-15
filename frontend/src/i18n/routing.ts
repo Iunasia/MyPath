5941e6946e8518b9ca7c27ee1d9dca0a095833f0
SeTh <panhaseth.sophat@outlook.com>
2026-09-15 15:38:31 +0700

    fix: update i18n routing config

--- frontend/src/i18n/routing.ts
+++ frontend/src/i18n/routing.ts
@@ -3,7 +3,7 @@ import {defineRouting} from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['en', 'km'],
   defaultLocale: 'en',
-  localeDetection: false
+  localeDetection: true
 });
 
 export type Locale = (typeof routing.locales)[number];
