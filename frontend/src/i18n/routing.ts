ef650ba5f24d97bea0e742d9479845f914917558
SeTh <panhaseth.sophat@outlook.com>
2026-09-15 10:28:17 +0700

    feat(i18n): add routing, request config, and locale messages

---
+++ frontend/src/i18n/routing.ts
@@ -0,0 +1,9 @@
+import {defineRouting} from 'next-intl/routing';
+
+export const routing = defineRouting({
+  locales: ['en', 'km'],
+  defaultLocale: 'en',
+  localeDetection: false
+});
+
+export type Locale = (typeof routing.locales)[number];
